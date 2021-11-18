84e36038c1d87f9327450c569e9bd16c3930d702
lindeneg <christian@lindeneg.org>
2021-11-18 20:14:42 +0100

    items test flle

---
+++ src/blog/__tests__/BlogWithImageItems.test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import userEvent from "@testing-library/user-event";
+import { render, queryByAttribute } from "@testing-library/react";
+import { BlogWithImageItems } from "../BlogWithImageItems";
+
+describe("BlogWithImageItems", () => {
+  test("renders component", () => {});
+});
