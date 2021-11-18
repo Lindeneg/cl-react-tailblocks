de036d64ab856d4758684ef8ab75b7f15df068e6
Christian <hi@lindeneg.org>
2021-11-18 20:10:10 +0100

    add-jest (#2)

---
+++ src/blog/__tests__/BlogWithImageItem.test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import userEvent from "@testing-library/user-event";
+import { render, queryByAttribute } from "@testing-library/react";
+import { BlogWithImageItem } from "../BlogWithImageItem";
+
+describe("BlogWithImageItem", () => {
+  test("renders component", () => {});
+});
