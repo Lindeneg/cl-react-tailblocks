95cf4b885a606ff7d3f1a2ea3e4ca1a236b5dbbc
lindeneg <christian@lindeneg.org>
2021-11-18 18:48:18 +0100

    implement learn more component

--- src/util/LearnMore.tsx
+++ src/util/LearnMore.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { WithTheme } from "../types";
 
-export type LearnMoreProps = {};
+export type LearnMoreProps = WithTheme<{
+  onClick?: (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
+  linkText?: string;
+}>;
 
-export function LearnMore({}: LearnMoreProps) {
-  return <div></div>;
+export function LearnMore({
+  onClick,
+  theme = "light",
+  linkText = "Learn More",
+}: LearnMoreProps) {
+  const isDark = theme === "dark";
+  return (
+    <a
+      onClick={onClick}
+      className={`cursor-pointer text-indigo-${
+        isDark ? "400" : "500"
+      } hover:text-indigo-${
+        isDark ? "500" : "600"
+      } inline-flex items-center md:mb-2 lg:mb-0`}
+    >
+      {linkText}
+      <svg
+        className="w-4 h-4 ml-2"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path d="M5 12h14"></path>
+        <path d="M12 5l7 7-7 7"></path>
+      </svg>
+    </a>
+  );
 }
