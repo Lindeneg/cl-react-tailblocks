79b22c118bff2d68773805ad9eba3918dba0614e
lindeneg <christian@lindeneg.org>
2021-11-19 07:03:05 +0100

    blog with avatar

--- src/blog/BlogWithAvatarItem.tsx
+++ src/blog/BlogWithAvatarItem.tsx
@@ -1,39 +1,61 @@
 import React from "react";
-import { LearnMore } from "..";
-import { Avatar } from "../util/Avatar";
+import { BlogItemProps, LearnMore } from "..";
+import { Avatar, AvatarProps } from "../util/Avatar";
 import { Stat } from "../util/Stat";
+import { WithoutTheme } from "../types";
 
-export type BlogWithAvatarItemProps = {};
+export type BlogWithAvatarItemProps = BlogItemProps & WithoutTheme<AvatarProps>;
 
-export function BlogWithAvatarItem({}: BlogWithAvatarItemProps) {
+export function BlogWithAvatarItem({
+  label,
+  title,
+  content,
+  linkNode,
+  linkText,
+  onClick,
+  views,
+  comments,
+  theme = "light",
+  includeWrapperBg = true,
+  ...avatarProps
+}: BlogWithAvatarItemProps) {
+  const isDark = theme === "dark";
   return (
-    <div className="p-12 md:w-1/2 flex flex-col items-start">
-      <span className="inline-block py-1 px-2 rounded bg-indigo-50 text-indigo-500 text-xs font-medium tracking-widest">
-        CATEGORY
+    <div
+      className={`p-12 md:w-1/2 flex flex-col items-start ${
+        isDark && includeWrapperBg ? "text-gray-400 bg-gray-900" : ""
+      }`}
+    >
+      <span
+        className={`inline-block py-1 px-2 rounded ${
+          isDark ? "bg-gray-800 text-gray-400" : "bg-indigo-50 text-indigo-500"
+        } text-xs font-medium tracking-widest`}
+      >
+        {label}
       </span>
-      <h2 className="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 mb-4">
-        Roof party normcore before they sold out, cornhole vape
+      <h2
+        className={`sm:text-3xl text-2xl title-font font-medium ${
+          isDark ? "text-white" : "text-gray-900"
+        } mt-4 mb-4`}
+      >
+        {title}
       </h2>
-      <p className="leading-relaxed mb-8">
-        Live-edge letterpress cliche, salvia fanny pack humblebrag narwhal
-        portland. VHS man braid palo santo hoodie brunch trust fund. Bitters
-        hashtag waistcoat fashion axe chia unicorn. Plaid fixie chambray 90's,
-        slow-carb etsy tumeric. Cray pug you probably haven't heard of them
-        hexagon kickstarter craft beer pork chic.
-      </p>
-      <div className="flex items-center flex-wrap pb-4 mb-4 border-b-2 border-gray-100 mt-auto w-full place-content-between">
-        <LearnMore />
-        <div className="mt-2">
-          <Stat views={1200} comments={6} />
+      <p className="leading-relaxed mb-8">{content}</p>
+      <div
+        className={`flex items-baseline sm:items-center sm:flex-row flex-col flex-wrap pb-4 mb-4 border-b-2 border-gray-${
+          isDark ? "800" : "100"
+        }  w-full place-content-between`}
+      >
+        {linkNode ? (
+          linkNode
+        ) : (
+          <LearnMore onClick={onClick} theme={theme} linkText={linkText} />
+        )}
+        <div className="mt-2 sm:mt-0">
+          <Stat views={views} comments={comments} theme={theme} />
         </div>
       </div>
-      <Avatar
-        imgSrc="https://dummyimage.com/104x104"
-        height={12}
-        width={12}
-        name="Holden Caulfield"
-        role="UI DEVELOPER"
-      />
+      <Avatar {...avatarProps} theme={theme} />
     </div>
   );
 }
--- src/blog/BlogWithAvatarItems.tsx
+++ src/blog/BlogWithAvatarItems.tsx
@@ -1,12 +1,32 @@
 import React from "react";
+import {
+  BlogWithAvatarItem,
+  BlogWithAvatarItemProps,
+} from "./BlogWithAvatarItem";
+import { WithTheme, WithoutTheme } from "../types";
 
-export type BlogWithAvatarItemsProps = {};
+export type BlogWithAvatarItemsProps = WithTheme<{
+  data: Array<WithoutTheme<BlogWithAvatarItemProps, "includeWrapperBg">>;
+}>;
 
-export function BlogWithAvatarItems({}: BlogWithAvatarItemsProps) {
+export function BlogWithAvatarItems({ data, theme }: BlogWithAvatarItemsProps) {
   return (
-    <section className="text-gray-600 body-font overflow-hidden">
+    <section
+      className={`${
+        theme === "dark" ? "text-gray-400 bg-gray-900" : "text-gray-600"
+      } body-font overflow-hidden`}
+    >
       <div className="container px-5 py-24 mx-auto">
-        <div className="flex flex-wrap -m-12"></div>
+        <div className="flex flex-wrap -m-12">
+          {data.map((entry, idx) => (
+            <BlogWithAvatarItem
+              {...entry}
+              theme={theme}
+              includeWrapperBg={false}
+              key={entry.label + idx}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
