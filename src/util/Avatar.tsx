bcb0fb902a30d187672eeaabd7a408db5b31dd78
lindeneg <christian@lindeneg.org>
2021-11-19 07:03:38 +0100

    implement avatar component

--- src/util/Avatar.tsx
+++ src/util/Avatar.tsx
@@ -1,13 +1,13 @@
 import React from "react";
+import { WithTheme, Image } from "../types";
 
-export type AvatarProps = {
+export type AvatarProps = WithTheme<{
   height: number;
   width: number;
   name: string;
   role: string;
-  imgSrc: string;
-  imgAlt?: string;
-};
+}> &
+  Image;
 
 export function Avatar({
   height,
@@ -15,8 +15,10 @@ export function Avatar({
   name,
   role,
   imgSrc,
+  theme = "light",
   imgAlt = "avatar-image",
 }: AvatarProps) {
+  const isDark = theme === "dark";
   return (
     <a className="inline-flex items-center">
       <img
@@ -25,8 +27,18 @@ export function Avatar({
         className={`w-${width} h-${height} rounded-full flex-shrink-0 object-cover object-center`}
       />
       <span className="flex-grow flex flex-col pl-4">
-        <span className="title-font font-medium text-gray-900">{name}</span>
-        <span className="text-gray-400 text-xs tracking-widest mt-0.5">
+        <span
+          className={`title-font font-medium ${
+            isDark ? "text-white" : "text-gray-900"
+          }`}
+        >
+          {name}
+        </span>
+        <span
+          className={`text-gray-${
+            isDark ? "500" : "400"
+          } text-xs tracking-widest mt-0.5`}
+        >
           {role}
         </span>
       </span>
