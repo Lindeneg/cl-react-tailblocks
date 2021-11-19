b9ee0f588e72851fe15c98780cedacefa39bddd6
lindeneg <christian@lindeneg.org>
2021-11-19 07:02:50 +0100

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { LearnMore } from "./util/LearnMore";
 export { Stat } from "./util/Stat";
 export { Button } from "./util/Button";
+export { Avatar } from "./util/Avatar";
 export { BlogItem } from "./blog/BlogItem";
 export { BlogItems } from "./blog/BlogItems";
 export { BlogWithImageItem } from "./blog/BlogWithImageItem";
@@ -12,6 +13,7 @@ export type { Theme, WithTheme, WithoutTheme } from "./types";
 export type { LearnMoreProps } from "./util/LearnMore";
 export type { StatProps } from "./util/Stat";
 export type { ButtonProps } from "./util/Button";
+export type { AvatarProps } from "./util/Avatar";
 export type { BlogItemProps } from "./blog/BlogItem";
 export type { BlogItemsProps } from "./blog/BlogItems";
 export type { BlogWithImageItemProps } from "./blog/BlogWithImageItem";
