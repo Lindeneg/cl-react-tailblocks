eb95e461c4ee695f0ae12f3658d3dd61634eacb6
lindeneg <christian@lindeneg.org>
2021-11-19 07:29:12 +0100

    avatar stories

---
+++ src/util/__stories__/Avatar.stories.tsx
@@ -0,0 +1,25 @@
+import { Story, Meta } from "@storybook/react/types-6-0";
+import { MOCK_ENTRY_WITH_AVATAR } from "../../blog/__stories__/mock-data";
+import { Avatar, AvatarProps } from "../Avatar";
+
+export default {
+  title: "Util/Avatar",
+  component: Avatar,
+} as Meta;
+
+const MetaComp: Story<AvatarProps> = (args) => <Avatar {...args} />;
+
+export const Light = MetaComp.bind({});
+Light.args = {
+  theme: "light",
+  ...MOCK_ENTRY_WITH_AVATAR,
+};
+
+export const Dark = MetaComp.bind({});
+Dark.args = {
+  theme: "dark",
+  ...MOCK_ENTRY_WITH_AVATAR,
+};
+Dark.parameters = {
+  backgrounds: { default: "dark" },
+};
