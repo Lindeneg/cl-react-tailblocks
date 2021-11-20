a241646f20431e5cb4cd18525b72e7cbd4794dcd
lindeneg <christian@lindeneg.org>
2021-11-20 23:34:06 +0100

    temp contact stories

--- src/contact/__stories__/ContactWithMapSmall.stories.tsx
+++ src/contact/__stories__/ContactWithMapSmall.stories.tsx
@@ -1,16 +1,16 @@
 import { Story, Meta } from "@storybook/react/types-6-0";
 import {
-  ContactWithMapBig,
-  ContactWithMapBigProps,
-} from "../ContactWithMapBig";
+  ContactWithMapSmall,
+  ContactWithMapSmallProps,
+} from "../ContactWithMapSmall";
 
 export default {
-  title: "Contact/ContactWithMapBig",
-  component: ContactWithMapBig,
+  title: "Contact/ContactWithMapSmall",
+  component: ContactWithMapSmall,
 } as Meta;
 
-const MetaComp: Story<ContactWithMapBigProps> = (args) => (
-  <ContactWithMapBig {...args} />
+const MetaComp: Story<ContactWithMapSmallProps> = (args) => (
+  <ContactWithMapSmall {...args} />
 );
 
 export const Light = MetaComp.bind({});
@@ -22,6 +22,10 @@ Light.args = {
   bottomDescription:
     "Chicharrones blog helvetica normcore iceland tousled brook viral artisan.",
   buttonLabel: "Submit",
+  address:
+    "Photo booth tattooed prism, portland taiyaki hoodie neutra typewriter",
+  email: "example@email.com",
+  phone: "123-456-7890",
   mapIframeSrc:
     "https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d71967.57989736123!2d12.507545599999998!3d55.689216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e2!4m0!4m5!1s0x465253a7a08b0e87%3A0x2e01262ccc705c92!2sJulius%20Thomsens%20Pl.%201%2C%201925%20Frederiksberg!3m2!1d55.681291599999994!2d12.5525136!5e0!3m2!1sen!2sdk!4v1637384541323!5m2!1sen!2sdk",
 };
