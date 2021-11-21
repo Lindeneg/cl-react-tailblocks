524e2fa70d1cc3b89633e1b9a928d1fd2190095f
lindeneg <christian@lindeneg.org>
2021-11-21 16:58:41 +0100

    start on components

---
+++ src/content/ContentWithIconItem.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export type ContentWithIconItemProps = {};
+
+export function ContentWithIconItem({}: ContentWithIconItemProps) {
+  return <div></div>;
+}
---
+++ src/content/ContentWithIconItems.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export type ContentWithIconItemsProps = {};
+
+export function ContentWithIconItems({}: ContentWithIconItemsProps) {
+  return <div></div>;
+}
