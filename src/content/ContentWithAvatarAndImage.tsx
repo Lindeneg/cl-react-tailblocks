3cdfa7cc06221177d3f7e9a31b50893b323d9b39
lindeneg <christian@lindeneg.org>
2021-11-21 22:57:36 +0100

    last two content components

---
+++ src/content/ContentWithAvatarAndImage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export type ContentWithAvatarAndImageProps = {};
+
+export function ContentWithAvatarAndImage({}: ContentWithAvatarAndImageProps) {
+  return <div></div>;
+}
---
+++ src/content/ContentWithImageBigItem.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export type ContentWithImageBigItemProps = {};
+
+export function ContentWithImageBigItem({}: ContentWithImageBigItemProps) {
+  return <div></div>;
+}
