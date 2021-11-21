20cddfd1e55ce0424c4b716535a0178b3e121920
lindeneg <christian@lindeneg.org>
2021-11-21 05:59:13 +0100

    fix entry attr

--- src/util/Socials.tsx
+++ src/util/Socials.tsx
@@ -116,7 +116,10 @@ export function Socials({
         </IconWrapper>
       )}
       {mail && (
-        <IconWrapper className="ml-4" entry={`mailto:${mail}`}>
+        <IconWrapper
+          className="ml-4"
+          entry={`mailto:${typeof mail === "string" ? mail : mail.href}`}
+        >
           <path
             data-testid="mail-path"
             d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"
