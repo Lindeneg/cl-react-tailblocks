bc5e0d03d56132d19a5901a5153cf11822cdbe6d
lindeneg <christian@lindeneg.org>
2021-11-23 02:22:09 +0100

    begin on last two footers

---
+++ src/footer/FooterWithLinksAndInputBig.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export type FooterWithLinksAndInputBigProps = {};
+
+export function FooterWithLinksAndInputBig({}: FooterWithLinksAndInputBigProps) {
+  return <div></div>;
+}
---
+++ src/footer/FooterWithLinksAndInputSmall.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export type FooterWithLinksAndInputSmallProps = {};
+
+export function FooterWithLinksAndInputSmall({}: FooterWithLinksAndInputSmallProps) {
+  return <div></div>;
+}
