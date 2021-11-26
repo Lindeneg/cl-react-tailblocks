463f3e38fc1a3d6babf0e9394c6daf793be809b2
lindeneg <christian@lindeneg.org>
2021-11-26 08:02:57 +0100

    mock data

---
+++ src/pricing/__stories__/mock-data.ts
@@ -0,0 +1,103 @@
+import { Prices } from "../PricingCard";
+
+export const MOCK_DATA_CARD = {
+  features: [
+    "Vexillologist pitchfork",
+    "Tumeric plaid portland",
+    "Mixtape chillwave tumeric",
+  ],
+  bottomText: "Literally you probably haven't heard of them jean shorts.",
+  label: "START",
+  price: "Free",
+};
+
+export const MOCK_DATA_CARDS = {
+  data: [
+    {
+      features: [
+        "Vexillologist pitchfork",
+        "Tumeric plaid portland",
+        "Mixtape chillwave tumeric",
+      ],
+      bottomText: "Literally you probably haven't heard of them jean shorts.",
+      label: "START",
+      prices: {
+        price: "Free",
+      },
+      selected: false,
+    },
+    {
+      features: [
+        "Vexillologist pitchfork",
+        "Tumeric plaid portland",
+        "Mixtape chillwave tumeric",
+        "Vexillologist pitchfork",
+      ],
+      bottomText: "Literally you probably haven't heard of them jean shorts.",
+      label: "PRO",
+      prices: [
+        {
+          price: "$38",
+          per: "/month",
+        },
+        {
+          price: "$400",
+          per: "/year",
+        },
+      ] as [Prices, Prices],
+      comparisons: ["", "$456"] as [string, string],
+      selected: false,
+    },
+    {
+      features: [
+        "Vexillologist pitchfork",
+        "Tumeric plaid portland",
+        "Mixtape chillwave tumeric",
+        "Vexillologist pitchfork",
+        "Vexillologist pitchfork",
+      ],
+      bottomText: "Literally you probably haven't heard of them jean shorts.",
+      label: "BUSINESS",
+      prices: [
+        {
+          price: "$56",
+          per: "/month",
+        },
+        {
+          price: "$600",
+          per: "/year",
+        },
+      ] as [Prices, Prices],
+      comparisons: ["", "$672"] as [string, string],
+      selected: true,
+    },
+    {
+      features: [
+        "Vexillologist pitchfork",
+        "Tumeric plaid portland",
+        "Mixtape chillwave tumeric",
+        "Vexillologist pitchfork",
+        "Vexillologist pitchfork",
+        "Vexillologist pitchfork",
+      ],
+      bottomText: "Literally you probably haven't heard of them jean shorts.",
+      label: "SPECIAL",
+      prices: [
+        {
+          price: "$72",
+          per: "/month",
+        },
+        {
+          price: "$750",
+          per: "/year",
+        },
+      ] as [Prices, Prices],
+      comparisons: ["", "$864"] as [string, string],
+      selected: false,
+    },
+  ],
+  description:
+    "Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical.",
+  title: "Pricing",
+  options: ["Monthly", "Annually"] as [string, string],
+};
