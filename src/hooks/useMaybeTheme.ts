cdfb2bf542b27a35062248cf0f414a358ac991a2
lindeneg <christian@lindeneg.org>
2021-12-01 18:47:43 +0100

    rename hook

---
+++ src/hooks/useMaybeTheme.ts
@@ -0,0 +1,29 @@
+import { useContext } from "react";
+import { ThemeContext, ThemeContextType } from "../context/ThemeContext";
+import { Theme, ThemeColor } from "../types";
+
+export function useMaybeTheme(
+  initial: Partial<ThemeContextType>
+): ThemeContextType {
+  const cxt = useContext(ThemeContext);
+
+  let t: Theme;
+  let c: ThemeColor;
+
+  if (initial.theme) {
+    t = initial.theme;
+  } else if (cxt?.theme) {
+    t = cxt.theme;
+  } else {
+    t = "light";
+  }
+  if (initial.color) {
+    c = initial.color;
+  } else if (cxt?.color) {
+    c = cxt.color;
+  } else {
+    c = "indigo";
+  }
+
+  return { theme: t, color: c, setContext: () => {} };
+}
