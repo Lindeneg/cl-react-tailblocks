13baecbf0671bdd1be9e6af734c5ad7abee99d28
lindeneg <christian@lindeneg.org>
2021-12-01 18:47:54 +0100

    context value function

--- src/context/ThemeContext.ts
+++ src/context/ThemeContext.ts
@@ -4,6 +4,7 @@ import { Theme, ThemeColor } from "../types";
 export type ThemeContextType = {
   theme: Theme;
   color: ThemeColor;
+  setContext: (newState: { theme?: Theme; color?: ThemeColor }) => void;
 };
 
 export const ThemeContext = createContext<ThemeContextType | null>(null);
