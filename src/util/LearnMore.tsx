21b23d27a8d003ce9de6acf3413b672ddcaf7c64
lindeneg <christian@lindeneg.org>
2021-12-03 00:46:35 +0100

    learnmore component should take optional linkNode

--- src/util/LearnMore.tsx
+++ src/util/LearnMore.tsx
@@ -7,38 +7,46 @@ export type LearnMoreProps = WithTheme<{
   onClick?: (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
   linkText?: string;
   extendClass?: string;
+  linkNode?: React.ReactNode;
 }>;
 
 export function LearnMore({
   onClick,
+  linkNode,
   extendClass = "",
   linkText = "Learn More",
   ...props
 }: LearnMoreProps) {
   const { theme, color } = useMaybeTheme(props);
   return (
-    <a
-      onClick={onClick}
-      className={getClass(
-        theme,
-        `cursor-pointer inline-flex items-center md:mb-2 lg:mb-0 ${extendClass}`,
-        `text-${color}-500 hover:text-${color}-600`,
-        `text-${color}-400 hover:text-${color}-500`
+    <>
+      {linkNode ? (
+        <>{linkNode}</>
+      ) : (
+        <a
+          onClick={onClick}
+          className={getClass(
+            theme,
+            `cursor-pointer inline-flex items-center md:mb-2 lg:mb-0 ${extendClass}`,
+            `text-${color}-500 hover:text-${color}-600`,
+            `text-${color}-400 hover:text-${color}-500`
+          )}
+        >
+          {linkText}
+          <svg
+            className="w-4 h-4 ml-2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M5 12h14"></path>
+            <path d="M12 5l7 7-7 7"></path>
+          </svg>
+        </a>
       )}
-    >
-      {linkText}
-      <svg
-        className="w-4 h-4 ml-2"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        strokeWidth="2"
-        fill="none"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="M5 12h14"></path>
-        <path d="M12 5l7 7-7 7"></path>
-      </svg>
-    </a>
+    </>
   );
 }
